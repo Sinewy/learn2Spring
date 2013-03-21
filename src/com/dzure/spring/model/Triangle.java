5c94c2bbd79461e1af67f162b4a2a7404ca919f5
sinewy@gmx.com <tele5jur>
2013-03-21 15:21:02 +0100

    Injecting with constructor, multiple constructors, spring automatically converts the type. If multiple constructors with different types are present, you can define property type to help Spring which constructor to use. You can also specify the index of the prameter.

--- src/com/dzure/spring/model/Triangle.java
+++ src/com/dzure/spring/model/Triangle.java
@@ -9,11 +9,30 @@ package com.dzure.spring.model;
 public class Triangle {
 
     private String type;
+    private int height;
+
+    // constructor injection
+    public Triangle(String type) {
+        this.type = type;
+    }
+
+    public Triangle(int height) {
+        this.height = height;
+    }
+
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 
     public String getType() {
         return type;
     }
-
+    //setter injection
     public void setType(String type) {
         this.type = type;
     }
@@ -21,5 +40,6 @@ public class Triangle {
     public void draw() {
         System.out.println("Triangle was drawn.");
         System.out.println("Triangle type: " + getType());
+        System.out.println("Triangle heigth: " + getHeight());
     }
 }
