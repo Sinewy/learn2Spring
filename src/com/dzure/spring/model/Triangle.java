60bc0463c81cc884dd32978d9dcf8edf4f60ae4b
sinewy@gmx.com <tele5jur>
2013-03-21 16:56:32 +0100

    Autowireing and different types.

--- src/com/dzure/spring/model/Triangle.java
+++ src/com/dzure/spring/model/Triangle.java
@@ -1,7 +1,5 @@
 package com.dzure.spring.model;
 
-import java.util.List;
-
 /**
  * Created with IntelliJ IDEA.
  * User: jurez
@@ -10,21 +8,38 @@ import java.util.List;
  */
 public class Triangle {
 
-    private List<Point> points;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
-    public List<Point> getPoints() {
-        return points;
+    public Point getPointA() {
+        return pointA;
     }
 
-    public void setPoints(List<Point> points) {
-        this.points = points;
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
     }
 
     public void draw() {
         System.out.println("Triangle was drawn.");
-
-        for (Point point : points) {
-            System.out.println("Point : " + point.getX() + ", " + point.getY());
-        }
+        System.out.println("Point A: " + getPointA().getX() + ", " + getPointA().getY());
+        System.out.println("Point B: " + getPointB().getX() + ", " + getPointB().getY());
+        System.out.println("Point C: " + getPointC().getX() + ", " + getPointC().getY());
     }
 }
