9e0df85641e94dd90463aeceb3e0a9fa92a36126
sinewy@gmx.com <tele5jur>
2013-03-21 14:37:18 +0100

    Without using spring

---
+++ src/com/dzure/spring/MainSpring.java
@@ -0,0 +1,19 @@
+package com.dzure.spring;
+
+import com.dzure.spring.model.Triangle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jurez
+ * Date: 3/21/13
+ * Time: 2:32 PM
+ */
+public class MainSpring {
+
+    public static void main(String[] args) {
+
+        Triangle triangle = new Triangle();
+        triangle.draw();
+
+    }
+}
---
+++ src/com/dzure/spring/model/Triangle.java
@@ -0,0 +1,14 @@
+package com.dzure.spring.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jurez
+ * Date: 3/21/13
+ * Time: 2:33 PM
+ */
+public class Triangle {
+
+    public void draw() {
+        System.out.println("Triangle was drawn.");
+    }
+}
