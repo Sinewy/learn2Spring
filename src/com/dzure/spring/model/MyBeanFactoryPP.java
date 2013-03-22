2af69203eed33431654e60165caca1bc2fffdb35
sinewy@gmx.com <tele5jur>
2013-03-22 09:26:20 +0100

    Bean Factory Post Processor - writing our own and activating it

---
+++ src/com/dzure/spring/model/MyBeanFactoryPP.java
@@ -0,0 +1,19 @@
+package com.dzure.spring.model;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jurez
+ * Date: 3/22/13
+ * Time: 9:21 AM
+ */
+public class MyBeanFactoryPP implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        System.out.println("Bean Factory is beeing executed.");
+    }
+}
