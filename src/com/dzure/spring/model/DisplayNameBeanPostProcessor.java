0a8e0221428c13006982a955422917d016b2677a
sinewy@gmx.com <tele5jur>
2013-03-22 09:17:26 +0100

    Bean Post Processor - executes stuf in the init process

---
+++ src/com/dzure/spring/model/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,26 @@
+package com.dzure.spring.model;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jurez
+ * Date: 3/22/13
+ * Time: 9:02 AM
+ */
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        System.out.println("Before init - Bean name: " + s);
+        return o;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+        System.out.println("After init - Bean name: " + s);
+        return o;
+    }
+
+}
--- src/com/dzure/spring/model/Triangle.java
+++ src/com/dzure/spring/model/Triangle.java
@@ -1,8 +1,5 @@
 package com.dzure.spring.model;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
 /**
  * Created with IntelliJ IDEA.
  * User: jurez
@@ -10,12 +7,7 @@ import org.springframework.beans.factory.InitializingBean;
  * Time: 2:33 PM
  */
 
-/**
- * If both ways are used... both will get executed.
- * First the spring dependent method and after the method we defined.
- */
-
-public class Triangle implements InitializingBean, DisposableBean {
+public class Triangle {
 
     private Point pointA;
     private Point pointB;
@@ -50,42 +42,6 @@ public class Triangle implements InitializingBean, DisposableBean {
         System.out.println("Point A: " + getPointA().getX() + ", " + getPointA().getY());
         System.out.println("Point B: " + getPointB().getX() + ", " + getPointB().getY());
         System.out.println("Point C: " + getPointC().getX() + ", " + getPointC().getY());
-    }
-
-    /**
-     * With this approach we write our own methods and we are not dependent on spring.
-     * All we have to do is tell spring in its configuration which methods to call
-     * for init or destroy - check out the spring config.
-     */
-     /**
-     * This method will be called right after the bean is initialized.
-     */
-    public void myInit() {
-        System.out.println("Initializing bean for Triangle - my initi method.");
-    }
-
-    /**
-    * This method is called just before the bean is destroyed.
-    */
-    public void myDestroy() {
-        System.out.println("Destroyed bean for Triangle - my destroy method.");
-    }
-
-
-    /**
-     * This method will be called right after the bean is initialized.
-     */
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("Initializing bean for Triangle.");
-    }
-
-    /**
-     * This method is called just before the bean is destroyed.
-     */
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("The bean for Triangle just being destroyed.");
     }
 
 }
